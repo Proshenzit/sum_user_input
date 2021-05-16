b590485cd3fb62429af2db85739788b66bfc49e7
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-15 23:26:53 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,34 @@
+
+
+var m = parseInt(prompt("Enter  the first Number: "));
+
+var n =parseInt(prompt("Enter  the first Lastnumber: "));
+
+var sum =0;
+
+
+
+for(var x=m; x<=n; x=x+1){
+sum=sum+x;
+
+
+}
+
+document.write(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
